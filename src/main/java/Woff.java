ec56a8a042d0d932a95f0b2304d5e28ffb6a4d59
KhalimboevSardorbek <khalimboevsardorbek@gmail.com>
2020-12-10 18:38:29 +0300

    initial commit

---
+++ src/main/java/Woff.java
@@ -0,0 +1,29 @@
+import com.google.common.io.Files;
+import com.google.typography.font.sfntly.Font;
+import com.google.typography.font.sfntly.FontFactory;
+import com.google.typography.font.sfntly.data.WritableFontData;
+import com.google.typography.font.tools.conversion.woff.WoffWriter;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+
+public class Woff {
+    public static void main(String[] args) {
+        WoffWriter ww = new WoffWriter();
+        FontFactory fontFactory = FontFactory.getInstance();
+        byte[] bytes;
+        try {
+            File actualFile = new File("D:\\font.ttf");
+            bytes = Files.toByteArray(actualFile);
+            Font font = fontFactory.loadFonts(bytes)[0];
+            WritableFontData wfd = ww.convert(font);
+
+            FileOutputStream fs = new FileOutputStream(actualFile.getName().substring(0, actualFile.getName().lastIndexOf(".")) + ".woff");
+            wfd.copyTo(fs);
+            fs.close();
+        } catch (IOException e1) {
+            e1.printStackTrace();
+        }
+    }
+}
